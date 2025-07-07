3e3cee92d96d0765314923757d8760fdd0cfc4ce
Greg Jackson <gregtj30@gmail.com>
2025-07-06 21:17:27 -0400

    change favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -32,25 +32,8 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
-      },
-      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
-      { rel: "icon", href: "/favicon.ico" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: "stylesheet", href: interFontCss },
       { rel: "stylesheet", href: jetbrainsMonoFontCss },
     ],
